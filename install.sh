48f9a5c2dd6d23c182d2eb25753050da933dfe79
H4wk <quasand89@gmail.com>
2023-02-05 00:07:17 +0100

    123

--- install.sh
+++ install.sh
@@ -6,15 +6,11 @@ basedir=$(dirname "$0")
 cd "$basedir" || { echo "Could not cd to $basedir" >&2; exit 1; }
 
 # Create symlinks from HOME directory to dotfiles directory
-ln -sv "$PWD/bash/bashrc" "$HOME/.bashrc"
-ln -sv "$PWD//bash/profile" "$HOME/.profile"
-ln -sv "$PWD/git/gitconfig" "$HOME/.gitconfig"
-ln -sv "$PWD/vim/vimrc" "$HOME/.vimrc"
-ln -sv "$PWD/zsh/zshrc" "$HOME/.zshrc"
-# need to treat directories differently
-if [[ ! -L "$HOME/.zsh" ]]; then
-    ln -sv "$PWD/zsh/zsh" "$HOME/.zsh"
-fi
-if [[ ! -L "$HOME/.config/nvim" ]]; then
-    ln -sv "$PWD/nvim" "$HOME/.config/nvim"
-fi
+ln -svn "$PWD/bash/bashrc" "$HOME/.bashrc"
+ln -svn "$PWD//bash/profile" "$HOME/.profile"
+ln -svn "$PWD/git/gitconfig" "$HOME/.gitconfig"
+ln -svn "$PWD/vim/vimrc" "$HOME/.vimrc"
+ln -svn "$PWD/zsh/zshrc" "$HOME/.zshrc"
+ln -svn "$PWD/nvim" "$HOME/.config/nvim"
+ln -svn "$PWD/zsh/zsh" "$HOME/.zsh"
+ln -svn "$PWD/ssh/config" "$HOME/.ssh/config"
--- nvim/lua/h4wk/remap.lua
+++ nvim/lua/h4wk/remap.lua
@@ -1,6 +1,8 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>p", '"+p')
 vim.keymap.set("n", "<F9>", ":Black<CR>")
 
 
